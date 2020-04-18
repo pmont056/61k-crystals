2e8ea7d0e9ab56457fe4be5a38bc3a5f5237c515
Chad Curtis <ccurtis7@uw.edu>
2020-04-18 16:15:51 -0700

    Added skeleton functions for next week to reproduce one-hot encoding from paper.

---
+++ sixty2k_crystals/features.py
@@ -0,0 +1,39 @@
+def structure_features(element):
+    '''
+    Calculates the structure features for a single atom
+
+    This is a long description
+
+    Parameters
+    ----------
+
+
+    Returns
+    -------
+
+    Examples
+    --------
+
+    '''
+
+    if atom == "C":
+        sfeatures[0] = 1
+    elif atom == "N":
+        sfeatures[1] = 1
+
+    return sfeatures
+
+def other_features(atom):
+    '''
+
+    '''
+    return other_features
+
+def all_features(smiles)
+
+    for atom in smiles:
+        structure_features()
+
+        other_features()
+
+    return
